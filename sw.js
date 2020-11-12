a3d2f4cde6388ef3b374342939b53a9f63968ce8
Natacha Millard <n.gentils@gmail.com>
2020-11-12 14:12:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-self.addEventListener ('message', event=>{
-    this.clients.matchAll().then(clients=>{
-        clients.forEach(client=>client.postMessage('Enchanté, je suis le service worker')) ;
-    }) ;
-}) ;
-
-self.addEventListener('fetch', event=>{
-    event.respondWith(new Response('PWA!!!!')) ;
-}) ;
-
 self.addEventListener('install', event=>{
     event.waitUntil(
         caches.open('v1').then(function(cache) {
@@ -28,3 +18,14 @@ self.addEventListener('install', event=>{
         })
     ) ;
 }) ;
+
+self.addEventListener ('message', event=>{
+    this.clients.matchAll().then(clients=>{
+        clients.forEach(client=>client.postMessage('Enchanté, je suis le service worker')) ;
+    }) ;
+}) ;
+
+self.addEventListener('fetch', event=>{
+    event.respondWith(new Response('PWA!!!!')) ;
+}) ;
+
