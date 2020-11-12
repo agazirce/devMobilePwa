6c40fcd5ad4219a78ed5f462ff973fb00b005110
Natacha Millard <n.gentils@gmail.com>
2020-11-12 13:37:23 +0100

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener ('message', event=>{
+    this.clients.matchAll().then(clients=>{
+        clients.forEach(client=>client.postMessage('Enchanté, je suis le service worker')) ;
+    }) ;
+}) ;
+
+self.addEventListener('fetch', event=>{
+    event.respondWith(new Response('PWA!!!!')) ;
+}) ;
