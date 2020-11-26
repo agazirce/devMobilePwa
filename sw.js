49599e13b00021d81b21a286d6217b0a7c9d557e
Natacha Millard <n.gentils@gmail.com>
2020-11-26 13:02:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,6 +16,70 @@ const files = [
     'https://cdnjs.cloudflare.com/ajax/libs/localforage/1.7.3/localforage.min.js'
     ];
 
+self.addEventListener("install", e => {
+    caches.open(cacheName).then(cache => {
+        cache.addAll(files);
+    });
+});
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(
+                keyList.map(function(key) {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {console.log(e.request.url);});
+
+self.addEventListener("fetch", event => {
+    const url = event.request.url;
+
+    if (url.indexOf("https://cranky-mcnulty-e3f846.netlify.app/GalerieRepos/galerie.json") === 0) {
+        event.respondWith(
+            fetch(event.request).then(response => {
+                if (response.status === 200) {
+                    console.info("Formatting data");
+                    return response.json().then(json => {
+                        const formattedResponse = json.map(j => ({
+                            name: j.name,
+                            description: j.description || "",
+                            updated_at: j.updated_at
+                        }));
+
+                        return new Response(JSON.stringify(formattedResponse));
+                    });
+                }
+
+                else{
+                    console.error(
+                        "Service Worker",
+                        "Error when fetching",
+                        event.request.url
+                    );
+
+                    return response;
+                }
+
+            })
+        );
+    }
+    else {
+        event.respondWith(
+            caches
+                .open(cacheName)
+                .then(cache => cache.match(event.request))
+                .then(response => response || fetch(event.request))
+        );
+    }
+});
+
+/*
 self.addEventListener('install', event=>{
     caches.open(cacheName).then(cache => {
         cache.addAll(files);
@@ -64,4 +128,4 @@ self.addEventListener("activate", e => {
             );
         })
     );
-});
+});*/
