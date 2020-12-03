e4d47fb5782d6a31a29f72a3bcf83332e4901e9c
Natacha Millard <n.gentils@gmail.com>
2020-12-03 14:10:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,15 +46,26 @@ function create_row(liste, row) {
                 button.classList.add('btn-success')
                 button.textContent = 'favori';
 
-                // Création d'un objet FormData
+                let fetchData;
+                if (navigator.onLine) {
+                    fetchData = fetch('http://localhost:3000/favoris')
+                        .then((response) => response.json())
+                        .then((data) => localforage.setItem("data", data));
+                } else {
+                    fetchData = localforage.getItem("data");
+                }
+
+                fetchData.then((json) => afficher(json));
+
+                /*// Création d'un objet FormData
                 let data = new FormData();
                 // Ajout d'information dans l'objet
                 data.append("src", image.src);
-                // Création et configuration d'une requête HTTP POST vers le fichier post_form.php
+                // Création et configuration d'une requête HTTP POST vers le serveur
                 let req = new XMLHttpRequest();
                 req.open("POST", "http://localhost:3000/favoris");
                 // Envoi de la requête en y incluant l'objet
-                req.send(data);
+                req.send(data);*/
             }
         });
         card_body.appendChild(title);
