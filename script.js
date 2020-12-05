9853d56e0f5a574f9381333214bba13242d145d4
Natacha Millard <n.gentils@gmail.com>
2020-12-05 21:49:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function create_row(liste, row) {
                 let fetchData;
                 if (navigator.onLine) {
                     let options = {method:'POST',
-                                    body: {img: image.src}};
+                                    body: JSON.stringify({img: image.src})};
                     console.log(options);
                     fetchData = fetch('http://localhost:3000/favoris', options)
                         .then((favoris) => {
