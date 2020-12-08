c4e9138735993a912bb27abe03a6883370f0515f
Natacha Millard <n.gentils@gmail.com>
2020-12-08 18:33:16 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ServiceWorkerWebpackPlugin = require("Serviceworker-webpack-plugin");
+const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports= {
     entry: "./script.js",
+    mode: process.env.NODE_ENV !== "production" ? "production" : "development",
     output: {
         filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist"),
@@ -14,12 +15,14 @@ module.exports= {
         new ServiceWorkerWebpackPlugin({
             entry: path.join(__dirname, './sw.js'),
         }),
-        new CopyPlugin([
-            { from: "images", to: "images" },
-            { from: "_headers", to: "./" },
-            { from: "manifest.webmanifest", to: "./" },
-            { from: "images.json", to: "./" },
-        ]),
+        new CopyPlugin({
+            patterns: [
+                { from: './manifest.webmanifest', to: './manifest.webmanifest' },
+                { from: './script.js', to: './script.js' },
+                { from: './images', to: './images' },
+                { from: './GalerieRepos', to: './GalerieRepos' },
+            ]
+        }),
     ],
     devServer: {
         contentBase: "./dist",
